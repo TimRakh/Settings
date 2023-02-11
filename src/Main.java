bb3d026becdb6f3ad9a027339fe293e93542af32
Tim Rakh <nbveh0914@gmail.com>
2023-02-11 11:00:47 +0300

    new1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,32 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     static StringBuilder loga = new StringBuilder();
+
     public static void main(String[] args) {
 
+        List<String> directories = new ArrayList<>();
+        directories.addAll(Arrays.asList("D://Games//res", "D://Games//src", "D://Games//savegames",
+                "D://Games//temp", "D://Games//src//main", "D://Games//src//test", "D://Games//res//drawables",
+                "D://Games//res//vectors", "D://Games//res//icons"));
+
+        for (int i = 0; i < directories.size(); i++) {
+            createDirectory(directories.get(i));
+        }
 
 
-        File src = creatingFile("D://Games", "src");
-
-        File res = creatingFile("D://Games", "res");
-
-        File savegames = creatingFile("D://Games", "savegames");
-
-        File temp = creatingFile("D://Games", "temp");
-
-        File main = creatingFile("D://Games//src", "main");
-
-        File test = creatingFile("D://Games//src", "test");
-
-        File mainJava = creatFils("D://Games//src//main//Main.java","Main.java");
+        File mainJava = creatFils("D://Games//src//main//Main.java", "Main.java");
 
         File utils = creatFils("D://Games//src//main//Utils.java", "Utils.java");
 
-        File drawables = creatingFile("D://Games//res", "drawables");
-
-        File vectors = creatingFile("D://Games//res", "vectors");
-
-        File icons = creatingFile("D://Games//res", "icons");
-
         File tempFile = creatFils("D://Games//temp//temp.txt", "temp.txt");
 
-        try (FileWriter writer = new FileWriter("D://Games//temp//temp.txt",false)){
+        try (FileWriter writer = new FileWriter("D://Games//temp//temp.txt", false)) {
             writer.write(String.valueOf(loga));
             writer.flush();
         } catch (IOException e) {
@@ -52,9 +46,9 @@ public class Main {
     }
 
 
-    public static File creatingFile(String adress, String nazv) {
-        File fl = new File(adress, nazv);
-        if (fl.mkdir()) loga.append("Каталог ").append(nazv).append(" создан").append("\n");
+    public static File createDirectory(String adress) {
+        File fl = new File(adress);
+        if (fl.mkdir()) loga.append("Каталог ").append(adress).append(" создан").append("\n");
         return fl;
     }
 }
